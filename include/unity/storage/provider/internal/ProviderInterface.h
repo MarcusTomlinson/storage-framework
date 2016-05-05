897363ba3cab1ded26c36951f7660a234bb540ba
James Henstridge <james@jamesh.id.au>
2016-05-05 13:48:21 +0800

    Add QtDBus skeletons.

---
+++ include/unity/storage/provider/internal/ProviderInterface.h
@@ -0,0 +1,40 @@
+
+#include <QObject>
+#include <QList>
+
+namespace unity {
+namespace storage {
+namespace provider {
+namespace internal {
+
+struct ItemMetadata {
+};
+
+class ProviderInterface : public QObject{
+    Q_OBJECT
+
+    explicit ProviderInterface(QObject *parent=nullptr);
+    ~ProviderInterface();
+
+public Q_SLOTS:
+
+    QList<ItemMetadata> Roots();
+    QList<ItemMetadata> List(const QString &item_id, const QString &page_token, QString &next_token);
+    QList<ItemMetadata> Lookup(const QString &parent_id, const QString &name);
+    ItemMetadata Metadata(const QString &item_id);
+    ItemMetadata CreateFolder(const QString &parent_id, const QString &name);
+    QString CreateFile(const QString &parent_id, const QString &name, const QString &content_type, bool allow_overwrite, QDBusUnixFileDescriptor &upload_fd);
+    QString Update(const QString &item_id, const QString &old_etag, QDBusUnixFileDescriptor &upload_fd);
+    ItemMetadata FinishUpload(const QString &upload_id);
+    void CancelUpload(const QString &upload_id);
+    QString Download(const QString &item_id, QDBusUnixFileDescriptor &download_fd);
+    void FinishDownload(const QString &download_id);
+    void Delete(const QString &item_id);
+    ItemMetadata Move(const QString &item_id, const QString &new_parent_id, const QString &new_name);
+    ItemMetadata Copy(const QString &item_id, const QString &new_parent_id, const QString &new_name);
+};
+
+}
+}
+}
+}
---
+++ src/provider/internal/ProviderInterface.cpp
@@ -0,0 +1,83 @@
+#include <unity/storage/provider/internal/ProviderInterface.h>
+
+namespace unity {
+namespace storage {
+namespace provider {
+namespace internal {
+
+ProviderInterface::ProviderInterface(QObject *parent) : QObject(parent)
+{
+}
+
+ProviderInterface::~ProviderInterface() = default;
+
+QList<ItemMetadata> ProviderInterface::Roots()
+{
+    return {};
+}
+
+QList<ItemMetadata> ProviderInterface::List(const QString &item_id, const QString &page_token, QString &next_token)
+{
+    return {};
+}
+
+QList<ItemMetadata> ProviderInterface::Lookup(const QString &parent_id, const QString &name)
+{
+    return {};
+}
+
+ItemMetadata ProviderInterface::Metadata(const QString &item_id)
+{
+    return {};
+}
+
+ItemMetadata ProviderInterface::CreateFolder(const QString &parent_id, const QString &name)
+{
+    return {};
+}
+
+QString ProviderInterface::CreateFile(const QString &parent_id, const QString &name, const QString &content_type, bool allow_overwrite, QDBusUnixFileDescriptor &upload_fd)
+{
+    return "";
+}
+
+QString ProviderInterface::Update(const QString &item_id, const QString &old_etag, QDBusUnixFileDescriptor &upload_fd)
+{
+    return "";
+}
+
+ItemMetadata ProviderInterface::FinishUpload(const QString &upload_id)
+{
+}
+
+void ProviderInterface::CancelUpload(const QString &upload_id)
+{
+}
+
+QString ProviderInterface::Download(const QString &item_id, QDBusUnixFileDescriptor &download_fd)
+{
+    return "";
+}
+
+void ProviderInterface::FinishDownload(const QString &download_id)
+{
+}
+
+void ProviderInterface::Delete(const QString &item_id)
+{
+}
+
+ItemMetadata ProviderInterface::Move(const QString &item_id, const QString &new_parent_id, const QString &new_name)
+{
+    return {};
+}
+
+ItemMetadata ProviderInterface::Copy(const QString &item_id, const QString &new_parent_id, const QString &new_name)
+{
+    return {};
+}
+
+}
+}
+}
+}
