74b1bac93ae55768d5076a120e47067b1d5acf9f
James Henstridge <james@jamesh.id.au>
2016-11-02 17:32:53 +0800

    Add OneDrive, at Gary's request.

--- src/qt/client/internal/remote_client/RuntimeImpl.cpp
+++ src/qt/client/internal/remote_client/RuntimeImpl.cpp
@@ -50,6 +50,7 @@ static const map<QString, QString> BUS_NAMES =
     { "google-drive-scope", "com.canonical.StorageFramework.Provider.ProviderTest" },
     { "com.canonical.scopes.mcloud_mcloud_mcloud", "com.canonical.StorageFramework.Provider.McloudProvider" },
     { "storage-provider-owncloud", "com.canonical.StorageFramework.Provider.OwnCloud" },
+    { "storage-provider-onedrive", "com.canonical.StorageFramework.Provider.OnedriveProvider" },
 };
 
 }  // namespace
